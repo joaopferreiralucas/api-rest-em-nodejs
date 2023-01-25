25c061f367f49bd44a6d22b189bb5bf9725a7f92
Joao Ferreira Lucas <joaopferreiralucas@gmail.com>
2023-01-25 12:49:07 -0300

    add store function for aluno

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -6,6 +6,17 @@ class AlunoController {
 
     return res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const newAluno = await Aluno.create(req.body);
+      return res.json(newAluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -4,5 +4,6 @@ import alunoController from '../controllers/AlunoController';
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.post('/', alunoController.store);
 
 export default router;
